8284affd2bd29992a753ec5f08f6ed5fad326d67
Craft <william.craft@accenture.com>
2021-05-24 08:18:34 -0400

    program will ask for user's measurements

---
+++ Room_Generator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Room_Detail_Generator
+{
+    class Room_Generator
+    {
+        static void Main(string[] args)
+        {
+            // initialize variables
+            double length;
+            double width;
+            double height;
+            string choice;
+            Boolean loopCondition = false;
+
+            Console.WriteLine("Welcome to the room generator. Our measurements will be in inches.");
+
+            while (loopCondition != true)
+            {
+
+                Console.WriteLine("Please enter your length");
+                length = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Please enter your Width");
+                width = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Please enter your height");
+                height = Convert.ToDouble(Console.ReadLine());
+
+                loopCondition = true;
+            }
+
+            Console.WriteLine("Indication the loop was successfully broken");
+        }
+    }
+}
