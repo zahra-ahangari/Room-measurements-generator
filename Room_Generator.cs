1b96ae870d5f45fed314ec3578a884e66267c681
Craft <william.craft@accenture.com>
2021-05-24 09:28:29 -0400

    added comments

--- Room_Generator.cs
+++ Room_Generator.cs
@@ -18,9 +18,10 @@ namespace Room_Detail_Generator
 
             Console.WriteLine("Welcome to the room generator. Our measurements will be in inches.");
 
+            // While loop will keep looping until condition is set to true
             while (loopCondition != true)
             {
-
+                // Measurements are inputed. The area, perimeter, and volume are calculated
                 Console.WriteLine("Please enter your length");
                 length = Convert.ToDouble(Console.ReadLine());
 
@@ -38,6 +39,7 @@ namespace Room_Detail_Generator
                 Console.WriteLine("The perimeter of your room is: " + perimeter);
                 Console.WriteLine("The volume of your room is: " + volume);
 
+                // User is prompt to go through sequence again, otherwise program is terminated.
                 Console.ForegroundColor = ConsoleColor.Blue;
                 Console.WriteLine("Would you like to continue? input Y to go again, input N to exit program");
                 choice = Console.ReadLine();
